beeeaf3af3252a3112e5654680986048be7e2860
UtkarshShukla-dev01 <utkarshshukla.cse@gmail.com>
2025-04-04 01:18:07 +0530

    Time: 49 ms (61.88%), Space: 29 MB (70.11%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end()); // Sort the array to enable two-pointer approach
+
+        int n = nums.size();
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate values
+
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+
+                    // Skip duplicates for left and right pointers
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+                else if (sum < 0) {
+                    left++; // Increase sum by moving left pointer
+                }
+                else {
+                    right--; // Decrease sum by moving right pointer
+                }
+            }
+        }
+
+        return result;
+    }
+};
